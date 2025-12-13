282d0d55136e922c1c0cd0b626883ed4046858ec
valenottaviano <valentinottaviano@gmail.com>
2025-12-13 13:13:01 -0300

    fix: suspense for useSearchParams

--- app/(auth)/auth/login/page.tsx
+++ app/(auth)/auth/login/page.tsx
@@ -1,10 +1,13 @@
 import { UserRole } from "@prisma/client";
 import { LoginForm } from "../_components/login-form";
+import { Suspense } from "react";
 
 export default function RegisterPage() {
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
-      <LoginForm />
+      <Suspense fallback={<div>Cargando...</div>}>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 }
--- app/(auth)/auth/register/page.tsx
+++ app/(auth)/auth/register/page.tsx
@@ -1,10 +1,13 @@
 import { RegisterForm } from "../_components/register-form";
 import { UserRole } from "@prisma/client";
+import { Suspense } from "react";
 
 export default function RegisterPage() {
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
-      <RegisterForm role={UserRole.USER} />
+      <Suspense fallback={<div>Cargando...</div>}>
+        <RegisterForm role={UserRole.USER} />
+      </Suspense>
     </div>
   );
 }
